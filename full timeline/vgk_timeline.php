c6d00862dc931977856e48e3544abdbd3698d19d
henry-burke <123663094+henry-burke@users.noreply.github.com>
2023-07-24 13:19:18 -0700

    Add files via upload

---
+++ full timeline/vgk_timeline.php
@@ -0,0 +1,226 @@
+<?php
+header('X-Frame-Options: ALLOW-FROM hjttps://www.nhl.com/');
+?>
+
+<html>
+
+<head>
+    <title>Team History | Vegas Golden Knights</title>
+    <meta author="Henry Burke-Web Development Intern" charset="UTF-8">
+    <link rel="stylesheet" , href="vgk_timeline_style.css">
+    <script src="vgk_timeline.js" defer></script>
+</head>
+
+<body>
+    <h1>VEGAS GOLDEN KNIGHTS HISTORY</h1>
+
+    <div class="tl-wrapper">
+        <div class="input-container">
+            <h2 class="header-year-select active">2017-18</h2>
+            <div class="time-line before active"></div>
+            <button class="button-year-select active" aria-label="2017-18" href="#timeline-info-block"></button>
+            <div class="time-line after"></div>
+        </div>
+
+        <div class="input-container">
+            <h2 class="header-year-select">2018-19</h2>
+            <div class="time-line before"></div>
+            <button class="button-year-select" aria-label="2018-19" href="#timeline-info-block"></button>
+            <div class="time-line after"></div>
+        </div>
+
+        <div class="input-container">
+            <h2 class="header-year-select">2019-20</h2>
+            <div class="time-line before"></div>
+            <button class="button-year-select" aria-label="2019-20" href="#timeline-info-block"></button>
+            <div class="time-line after"></div>
+        </div>
+
+        <div class="input-container">
+            <h2 class="header-year-select">2020-21</h2>
+            <div class="time-line before"></div>
+            <button class="button-year-select" aria-label="2020-21" href="#timeline-info-block"></button>
+            <div class="time-line after"></div>
+        </div>
+
+        <div class="input-container">
+            <h2 class="header-year-select">2021-22</h2>
+            <div class="time-line before"></div>
+            <button class="button-year-select" aria-label="2021-22" href="#timeline-info-block"></button>
+            <div class="time-line after"></div>
+        </div>
+
+        <div class="input-container">
+            <h2 class="header-year-select">2022-23</h2>
+            <div class="time-line before"></div>
+            <button class="button-year-select" aria-label="2022-23" href="#timeline-info-block"></button>
+            <div class="time-line after"></div>
+        </div>
+
+        <div class="input-container">
+            <h2 class="header-year-select">2023-24</h2>
+            <div class="time-line before"></div>
+            <button class="button-year-select" aria-label="2023-24" href="#timeline-info-block"></button>
+            <div class="time-line after"></div>
+        </div>
+
+        <div class="input-container">
+            <h2 class="header-year-select">2024-25</h2>
+            <div class="time-line before"></div>
+            <button class="button-year-select" aria-label="2024-25" href="#timeline-info-block"></button>
+            <div class="time-line after"></div>
+        </div>
+
+        <div class="input-container">
+            <h2 class="header-year-select">2025-26</h2>
+            <div class="time-line before"></div>
+            <button class="button-year-select" aria-label="2025-26" href="#timeline-info-block"></button>
+            <div class="time-line after"></div>
+        </div>
+
+        <div class="input-container">
+            <h2 class="header-year-select">2026-27</h2>
+            <div class="time-line before"></div>
+            <button class="button-year-select" aria-label="2026-27" href="#timeline-info-block"></button>
+            <div class="time-line after"></div>
+        </div>
+    </div>
+    <div class="info-wrapper" id="timeline-info-block">
+        <div class="back-button-wrapper">
+            <button class="back-button">
+                <span>Back to Timeline</span>
+            </button>
+        </div>
+
+        <div class="year-info active">
+            <h2>Year One ~ 2017-18</h2>
+            <div class="img-container">
+                <img src="https://i.pinimg.com/originals/fe/1b/10/fe1b10f84e3eb7a24cf08dd3d04b439a.jpg">
+            </div>
+            <div class="time-post">
+                <div class="left reveal">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip
+                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                        dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                        deserunt mollit anim id est laborum.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip
+                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                        dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                        deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="dot"></div>
+                <div class="center-line"></div>
+                <div class="right reveal">
+
+                    <blockquote>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore
+                            et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                            ut
+                            aliquip
+                            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum
+                            dolore eu
+                            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                            officia
+                            deserunt mollit anim id est laborum.</p>
+                        <footer><cite>-Random Quote, 1903</cite></footer>
+                    </blockquote>
+
+                </div>
+            </div>
+
+            <div class="time-post">
+                <div class="left reveal">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip
+                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                        dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                        deserunt mollit anim id est laborum.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip
+                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                        dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                        deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="dot"></div>
+                <div class="center-line"></div>
+                <div class="right reveal">
+                    <h3>Expansion Draft</h3>
+                    <img src="https://cms.nhl.bamgrid.com/images/photos/290058920/1024x576/cut.jpg">
+                </div>
+            </div>
+            <div class="back-button-wrapper">
+                <button class="back-button">
+                    <span>Back to Timeline</span>
+                </button>
+            </div>
+        </div>
+
+
+        <div class="year-info">
+            <p>test2</p>
+        </div>
+
+
+        <div class="year-info">
+            <p>test3</p>
+        </div>
+
+
+        <div class="year-info">
+            <p>test4</p>
+        </div>
+
+
+        <div class="year-info">
+            <p>test5</p>
+        </div>
+
+
+        <div class="year-info">
+            <p>test6</p>
+        </div>
+
+
+        <div class="year-info">
+            <p>test7</p>
+        </div>
+
+
+        <div class="year-info">
+            <p>test8</p>
+        </div>
+
+
+        <div class="year-info">
+            <p>test9</p>
+        </div>
+
+
+        <div class="year-info">
+            <p>test10</p>
+        </div>
+    </div>
+</body>
+
+</html>
