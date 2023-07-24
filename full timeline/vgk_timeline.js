a3de5ebbe13cddfd515b91b8f484e7ab62284fd8
henry-burke <123663094+henry-burke@users.noreply.github.com>
2023-07-24 13:19:02 -0700

    added scroll and reveal animations

--- full timeline/vgk_timeline.js
+++ full timeline/vgk_timeline.js
@@ -135,3 +135,21 @@ const autoScroll = (element, e, top) => {
         behavior: "smooth"
     });
 }
+
+const reveal = () => {
+    let revealImg = document.querySelectorAll(".reveal");
+
+    for (let i = 0; i < revealImg.length; i++) {
+        let windowHeight = window.innerHeight;
+        let elementTop = revealImg[i].getBoundingClientRect().top;
+        let elementVisible = 175;
+
+        if (elementTop < (windowHeight - elementVisible)) {
+            revealImg[i].classList.add("active");
+        } else {
+            revealImg[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
