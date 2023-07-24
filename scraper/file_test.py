b06dbad4526799c142b09e24bf6b6cd4ff48057a
henry-burke <123663094+henry-burke@users.noreply.github.com>
2023-07-24 13:31:21 -0700

    Delete scraper directory

--- scraper/file_test.py
+++
@@ -1,9 +0,0 @@
-import os.path
-
-dir_name = os.path.dirname(__file__)
-file_name = os.path.join(dir_name, "output")
-
-if not os.path.exists(file_name):
-    os.mkdir(file_name)
-
-print(file_name)
